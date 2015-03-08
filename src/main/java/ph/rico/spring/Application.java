ab9bd43ba470fce4dc8611c91f7eff9df37b5947
Rico -  Desktop <federicoparassarmiento@yahoo.com>
2015-03-08 19:48:10 +0800

    up

--- src/main/java/ph/rico/spring/Application.java
+++ src/main/java/ph/rico/spring/Application.java
@@ -8,6 +8,17 @@ public class Application {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("bean0303-context.xml");
 		
-		Bean0303 bean = context.getBean("bean", Bean0303.class);
+		Bean0303 bean = context.getBean("_bean1", Bean0303.class);
+		Bean0303 bean2 = context.getBean("bean", Bean0303.class);
+		Bean0303 bean3 = context.getBean("bean2", Bean0303.class);
+		
+		//this will print true since bean are singleton and reference to 1 bean
+		System.out.println(bean == bean2);
+		
+		//this will print false since bean are singleton and reference to different bean
+		System.out.println(bean == bean3);
+				
+				
+		( (ClassPathXmlApplicationContext)context ).close();         
 	}
 }
--- src/main/java/ph/rico/spring/Bean0303.java
+++ src/main/java/ph/rico/spring/Bean0303.java
@@ -1,6 +1,6 @@
 package ph.rico.spring;
 
-//creating a bean
+//creating a spring bean
 public class Bean0303 {
 	public Bean0303() {
 		System.out.println("Bean0303 created");

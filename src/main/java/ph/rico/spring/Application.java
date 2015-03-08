a578947b572a7a1b5acf7779e8733a4bcb69ed32
Rico -  Desktop <federicoparassarmiento@yahoo.com>
2015-03-08 14:54:31 +0800

    Ini

---
+++ src/main/java/ph/rico/spring/Application.java
@@ -0,0 +1,10 @@
+package ph.rico.spring;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/ph/rico/spring/BeanA.java
@@ -0,0 +1,10 @@
+package ph.rico.spring;
+
+public class BeanA {
+	private int field1;
+
+	public BeanA() {
+		super();
+		System.out.println("BeanA initialized");
+	}
+}
